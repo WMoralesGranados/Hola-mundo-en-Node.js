14f4a4df4b41671bf812cbf154d084ba8b52e3ae
William Morales <williamorggan@gmail.com>
2024-04-18 19:17:47 -0600

    Hola mundo con Node JS

---
+++ holaMundoJS.js
@@ -0,0 +1,11 @@
+//Hola mundo con console.log
+console.log("Hola Mundo!");
+
+//Hola mundo con una funcion
+function holaMundo(str) {
+    return str;
+}
+
+//Hola mundo con variables
+var holaMundo = 'Hola Mundo!'
+console.log(holaMundo);
